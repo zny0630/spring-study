bc48cb51db5947513597760048a6decd41b5e82a
Your NameZny0630 <1624329560@qq.com>
2019-03-06 00:09:22 +0800

    my first commmit

--- IoC/src/main/java/com/spring/IoC/di/Application.java
+++ IoC/src/main/java/com/spring/IoC/di/Application.java
@@ -12,5 +12,6 @@ public class Application {
         ApplicationContext context =new ClassPathXmlApplicationContext("spring.xml");
         MessagePrinter printer=context.getBean(MessagePrinter.class);
         printer.pinterMessage();
+
     }
 }
