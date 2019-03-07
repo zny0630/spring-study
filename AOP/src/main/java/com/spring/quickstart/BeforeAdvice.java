ef37b0012b832d04faa4af8e960e651812b0b37c
zny0630 <1624329560@qq.com>
2019-03-07 19:38:22 +0800

    aop

--- AOP/src/main/java/com/spring/quickstart/BeforeAdvice.java
+++ AOP/src/main/java/com/spring/quickstart/BeforeAdvice.java
@@ -4,8 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BeforeAdvice {
-    private static final Logger logger= LoggerFactory.getLogger(UserDao.class);
-    //定义前置方法
+    private static final Logger logger= LoggerFactory.getLogger(BeforeAdvice.class);
     public void beforeMethod(){
         logger.info("连接数据库");
     }
--- AOP/src/main/java/com/spring/quickstart/UserDaoImpl.java
+++ AOP/src/main/java/com/spring/quickstart/UserDaoImpl.java
@@ -3,12 +3,10 @@ package com.spring.quickstart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UserDaoImpl implements UserDao{
-    private static final Logger logger= LoggerFactory.getLogger(UserDao.class);
-
+public class UserDaoImpl implements UserDao {
+    private static final Logger logger= LoggerFactory.getLogger(UserDaoImpl.class);
     @Override
     public void insert() {
-
-        logger.info("开始进入插入操作");
+        logger.info("开始进行插入操作");
     }
 }
